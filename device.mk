efd7d9e7ca16f954bb2988a9c4e126e599ab90cd
Michael Bestas <mkbestas@lineageos.org>
2024-11-02 17:03:23 +0530

    nabu: build libcrypto_shim for libwvhidl
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180

--- device.mk
+++ device.mk
@@ -228,7 +228,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
-    libcrypto-v33
+    libcrypto-v33 \
+    libcrypto_shim \
+    libcrypto_shim.vendor
 
 # fastbootd
 PRODUCT_PACKAGES += \
